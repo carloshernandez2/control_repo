1f8e00e52ad0860369cbf42a854477283c947c70
Carlos Hernández <75079099+carloshernandez2@users.noreply.github.com>
2021-12-10 16:51:17 -0300

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -7,14 +7,18 @@ class minecraft {
     source => 'https://launcher.mojang.com/v1/objects/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar',
   }
   package {'java':,
-   ensure => present
+   ensure => present,
   }
   file {'/opt/minecraft/eula.text'
     ensure => file,
-    content => 'eula=true'
+    content => 'eula=true',
   }
   file {'/etc/systemd/system/minecraft.service':
     ensure => file,
-    source => 'puppet:///modules/minecraft/minecraft.service'
+    source => 'puppet:///modules/minecraft/minecraft.service',
+  }
+  service {'minecraft':
+    ensure => running,
+    enable => true,
   }
 }
